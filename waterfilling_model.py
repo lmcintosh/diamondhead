9c55bcdb665e5c072c99973aefbe619642854b32
Lane McIntosh <lanemcintosh@gmail.com>
2014-09-03 17:45:58 -0700

    Updated model with separate function runModel, and revised parameters that are more generous.

--- waterfilling_model.py
+++ waterfilling_model.py
@@ -8,6 +8,50 @@ import pandas as pd
 from emailing import *
 
 
+def runModel(lam, alpha_d, beta_d, alpha_p, beta_p, monthly_shareprice):
+    # Define monthly household income for bank; will need to periodically update this manually
+    z = np.linspace(5.993,10.88,len(monthly_shareprice))
+    monthly_household_income = np.exp(z)
+
+    # Fit line to S&P500 percentage gains
+    x_mo  = np.linspace(0,len(monthly_shareprice),len(monthly_shareprice))
+    ps_mo = np.polyfit(x_mo,np.log(monthly_shareprice),deg=1)
+    fit_line_mo = np.exp(ps_mo[0]*x_mo + ps_mo[1] + lam)
+    
+    bank = 0.
+    shares = 0.
+    for m, price in enumerate(monthly_shareprice):
+        # receive your paycheck
+        bank = bank + 0.1*monthly_household_income[m]
+
+        # check how expensive/cheap the market is
+        # positive if below watermark
+        discount = (fit_line_mo[m]- price)/price
+
+        # allocate how much of bank you want to invest
+        if discount >= 0.0:
+            purchase = (alpha_d*discount + beta_d)*bank
+        else:
+            purchase = (alpha_p*discount + beta_p)*bank
+        if purchase > bank:
+            purchase = bank
+        elif purchase < 0.0:
+            purchase = 0.0
+
+        # buy shares
+        shares = shares + purchase/price
+        bank   = bank - purchase
+
+        # accrue interest in bank
+        bank   = bank + (0.01/12.)*bank
+
+    final_price  = list(monthly_shareprice)[-1]
+    fraction     = purchase/(0.1*monthly_household_income[m])
+    
+    return discount,fraction
+
+
+
 while True:
     d = datetime.date.today()
     yr_num   = d.year
@@ -22,7 +66,7 @@ while True:
         # Open stock price data
         data = pd.read_csv('sp500_history.csv')
 
-        monthly_shareprice = data['Close'][0::30]
+        monthly_shareprice = np.array(data['Close'])[0::30]
         monthly_shareprice = monthly_shareprice[::-1] # time=0 => oldest
         
         
@@ -33,23 +77,15 @@ while True:
         beta_d  = 0.26391542623164571
         beta_p  = -4.866531100483897e-05
 
-        # Fit line that gives the average return
-        x_mo  = np.linspace(0,len(monthly_shareprice),len(monthly_shareprice))
-        ps_mo = np.polyfit(x_mo,np.log(monthly_shareprice),deg=1)
-        fit_line_mo = np.exp(ps_mo[0]*x_mo + ps_mo[1] + lam)
-
-        # Compute current market conditions
-        price    = list(monthly_shareprice)[-1]
-        discount = (fit_line_mo[-1] - price)/price
+        # RUN MODEL
+        discount,fraction_to_buy = runModel(lam,alpha_d,beta_d,alpha_p,beta_p,monthly_shareprice,monthly_household_income)
 
         if discount >= 0.0:
-            fraction_to_buy = (alpha_d*discount + beta_d)
             market_condition = 'cheap'
-	    discount_or_premium = 'discount'
+            discount_or_premium = 'discount'
         else:
-            fraction_to_buy = (alpha_p*discount + beta_p)
             market_condition = 'expensive'
-	    discount_or_premium = 'premium'
+            discount_or_premium = 'premium'
 
         fraction_to_hold = 1.0 - fraction_to_buy
 
@@ -57,12 +93,12 @@ while True:
             fraction_to_buy = 0.0
         if fraction_to_hold < 0:
             fraction_to_hold = 0.0
-	if fraction_to_hold > 1.0:
-	    fraction_to_hold = 1.0
+        if fraction_to_hold > 1.0:
+            fraction_to_hold = 1.0
 
         msg = 'Today is %d-%d-%d and the S&P last closed at %d. Market is currently %s with %s %d%%. Buy %d%% stocks and keep %d%% cash.' % (mon_num,day_num,yr_num,price,market_condition, discount_or_premium, np.abs(100.*discount), 100.*fraction_to_buy, 100.*fraction_to_hold)
         emailMe(msg)
-	time.sleep(24*60*60) # Go to sleep after sending message
+        time.sleep(24*60*60) # Go to sleep after sending message
 
     time.sleep(24*60*60) # Sleep for a day before checking again
 
