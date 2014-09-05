42e0459e4844a410707b07866e7db0c9c238113f
Lane McIntosh <lanemcintosh@gmail.com>
2014-09-05 10:09:57 -0700

    Modified waterfilling_model_test so that it can email you once every weekday.  Previously it slept twice after sending message.

--- waterfilling_model_test.py
+++ waterfilling_model_test.py
@@ -58,7 +58,7 @@ while True:
     mon_num  = d.month
     day_num  = d.day
     day_name = d.weekday()
-    if day_num < 32:
+    if day_num < 32 and day_name < 5:
         # Fetch current price data from Yahoo Finance
         url = 'http://real-chart.finance.yahoo.com/table.csv?s=%%5EGSPC&d=%d&e=%d&f=%d&g=d&a=0&b=3&c=1950&ignore=.csv' % (mon_num,day_num,yr_num)
         urllib.urlretrieve(url,'sp500_history.csv')
@@ -99,6 +99,6 @@ while True:
         msg = 'Today is %d-%d-%d and the S&P last closed at %d. Market is currently %s with %s %d%%. Buy %d%% stocks and keep %d%% cash.' % (mon_num,day_num,yr_num,monthly_shareprice[-1],market_condition, discount_or_premium, np.abs(100.*discount), 100.*fraction_to_buy, 100.*fraction_to_hold)
         emailMe(msg)
         time.sleep(24*60*60) # Go to sleep after sending message
-
-    time.sleep(24*60*60) # Sleep for a day before checking again
+    else:
+        time.sleep(24*60*60) # Sleep for a day before checking again
 
